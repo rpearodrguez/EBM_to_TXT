1cf0388333744e38effc47a0cc60a1135a60b219
Richard Peña <richardignaciopr@gmail.com>
2025-02-10 09:49:26 -0300

    Añade documentación al código

--- ebm_to_txt.py
+++ ebm_to_txt.py
@@ -13,23 +13,31 @@ def extract_strings(file_name):
             with open(file_name.replace('.ebm', '.txt'), 'w') as new_file:
                 for line in lines:
                     new_line = ''
+                    # Crea un contador de caracteres y un string para almacenarlos temporalmente, su funcionalidad es evitar que se agreguen caracteres solitarios al archivo txt.
                     char_counter = 0
                     char_stock = ''
+                    # Recorre cada linea del archivo ebm
                     for char in line:
+                        # Busca caracter por caracter y si es alfanumérico o un caracter especial, lo añade al string temporal
                         if char.isalpha() or char.isdigit() or char in [' ', '.', ',', '!', '?', '\'', '\"',':', ';', '(', ')', '-', '_']:
+                            # Evita añadir el caracter ÿ al archivo txt, este caracter apareció bastante en los archivos .ebm utilizados para la prueba
                             if char == 'ÿ':
                                 continue
+                            # 
                             char_counter += 1
                             char_stock += char
+                        # Si es un salto de línea o tabulación, se añade directamente al nuevo archivo
                         elif char == '\n':
                             new_line += '\n'
                         elif char == '\t':
                             new_line += '\t'
+                        # Al momento de llegar a un caracter inválido, se añade el string temporal al nuevo archivo en caso de que el contador sea superior o igual a 2
                         else:
                             if char_counter >= 2:
                                 new_line += char_stock
                                 char_counter = 0
                                 char_stock = ''
+                            # Si el contador es menor a 2, se reinicia el contador y el string temporal
                             else:
                                 char_counter = 0
                                 char_stock = ''
